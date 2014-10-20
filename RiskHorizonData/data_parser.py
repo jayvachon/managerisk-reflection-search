862596186088ffea1e3ee0122768c91e19f93fd4
Jay Vachon <jaylvachon@gmail.com>
2014-10-20 16:50:27 -0400

    added average time spent playing per tier

--- RiskHorizonData/data_parser.py
+++ RiskHorizonData/data_parser.py
@@ -66,9 +66,38 @@ class GameDataParser:
 	def __init__(self, game_data, countryDataParser):
 		self.game_data = game_data
 		self.countryDataParser = countryDataParser
-		self.getHighestAverageLevelByCountry()
+		self.playersHighestLevels = self.getPlayersHighestLevel()
+		# self.getHighestAverageLevelByCountry()
+		# print self.getTimePlayedByIP(0)
+		# print self.getPlayersHighestLevel()
+		self.getTimesPlayedByTier()
 
-	def getHighestLevelByIP(self, index):
+	def getFinalLevels(self):
+		# returns an array of players' last levels
+		levelCounts = [0, 0, 0, 0, 0, 0]
+		for h in self.playersHighestLevels:
+			levelCounts[h['level'] - 1] += 1
+		return levelCounts
+
+	def getTierCounts(self):
+		finalLevels = self.getFinalLevels()
+		return [finalLevels[0] + finalLevels[1], finalLevels[2] + finalLevels[3], finalLevels[4] + finalLevels[5]]
+
+	def getTier(self, highestLevel):
+		"""
+		use this to divide players into 3 groups based on success in the game
+		1 is bottom 3rd,
+		2 is middle 3rd,
+		3 is top 3rd
+		"""
+		if highestLevel < 3:
+			return 1
+		elif highestLevel < 5:
+			return 2
+		else:
+			return 3
+
+	def getPlayerHighestLevel(self, index):
 		sessions = game_data[index]['session']
 		highestLevel = {}
 		highestLevel['ip'] = game_data[index]['ipv4']
@@ -81,27 +110,31 @@ class GameDataParser:
 					highestLevel['level'] = level
 		return highestLevel
 
-	def getHighestLevelsByIP(self):
+	def getPlayersHighestLevel(self):
+		# returns an array of players with the highest level they reached
 		highestLevels = []
 		for i in range(len(game_data)):
-			highestLevels.append(self.getHighestLevelByIP(i))
+			highestLevels.append(self.getPlayerHighestLevel(i))
 		return highestLevels
+
+	def findPlayerHighestLevel(self, ip):
+		for hl in self.playersHighestLevels:
+			if hl['ip'] == ip:
+				return hl['level']
+		return 'ip ' + ip + ' not found'
 
 	def getHighestAverageLevelByCountry(self):
 
 		# skip countries who had fewer than this many players
 		minPlayerCount = 30
 
-		# each player's highest level
-		highestLevels = self.getHighestLevelsByIP()
-
 		# an array of all countries with number of players
 		countries = self.countryDataParser.getCountries()
 
 		# make a dict with the total of max levels reached by all players in each country
 		self.countriesLevels = {}
-		for i in range(len(highestLevels)):
-			player = highestLevels[i]
+		for i in range(len(self.playersHighestLevels)):
+			player = self.playersHighestLevels[i]
 			ip = player['ip']
 			level = player['level']
 			country = self.countryDataParser.getCountryByIP(str(ip))
@@ -123,7 +156,48 @@ class GameDataParser:
 		self.avgLevels = self.avgLevels[::-1]
 		for a in self.avgLevels:
 			print str(a[0]) + ": " + str(a[1])
+
+	def getTimePlayedByIP(self, index):
+		# gets the total amount of time, in minutes, that a player played the game
+		sessions = game_data[index]['session']
+		timePlayed = {}
+		timePlayed['ip'] = game_data[index]['ipv4']
+		time = 0
+		levelDuration = 3 # minutes
+		for i in range(len(sessions)):
+			levels = sessions[i]['levels']
+			for l in levels:
+				time += levelDuration
 			
+		timePlayed['time'] = time
+		return timePlayed
+
+	def getTimesPlayedByTier(self):
+		plays = []
+
+		# makes an array with entries containing ip, tier, and total time played
+		for i in range(len(game_data)):
+			play = self.getTimePlayedByIP(i)
+			level = self.findPlayerHighestLevel(play['ip'])
+			play['tier'] = self.getTier(level)
+			plays.append(play)
+
+		totalTierTime = [0, 0, 0]
+		for p in plays:
+			tier = p['tier']
+			totalTierTime[tier - 1] += p['time']
+			if p['time'] < 3:
+				print p['time']
+
+		tierCounts = self.getTierCounts();
+		avgTimePlayedPerTier = []
+		for i in range(3):
+			avgTimePlayedPerTier.append(totalTierTime[i] / tierCounts[i])
+
+		print avgTimePlayedPerTier
+
+
+
 
 def getJSONData(path):
 	print 'loading ' + path + '...'
@@ -132,7 +206,7 @@ def getJSONData(path):
 	print path + ' loaded'
 	return d
 
-def sanitizeLocations():
+def sanitizeLocationData():
 	d = {}
 	d['locations'] = []
 	locations = locations_data['locations']
@@ -143,10 +217,24 @@ def sanitizeLocations():
 					d['locations'].append(location)
 	return d
 
+def sanitizeGameData():
+	g = []
+	emptySessionCount = 0
+	for i in range(len(game_data)):
+		session = game_data[i]['session']
+		# removes players who were not able to play the game
+		if len(session[0]['levels']) > 0:
+			g.append(game_data[i])
+		else:
+			emptySessionCount += 1
+	print str(emptySessionCount) + " empty sessions"
+	return g
+
 locations_data = getJSONData('locations.json')
-locations_data = sanitizeLocations()
+locations_data = sanitizeLocationData()
 
 game_data = getJSONData('json_parser/data/risk_horizon.json')
+game_data = sanitizeGameData()
 
 cdp = CountryDataParser(locations_data)
 gdp = GameDataParser(game_data, cdp)
